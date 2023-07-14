eb7b1860a52747681afad0b7702e0165d6a7757a
Jimmy-Sim <77947780+Jimmy-Sim@users.noreply.github.com>
2023-07-14 09:09:15 +0900

    Update IntroView.swift

--- Get Started with Apps/IntroView.swift
+++ Get Started with Apps/IntroView.swift
@@ -1,18 +1,7 @@
 import SwiftUI
 
-/*#-code-walkthrough(1.first)*/
-struct/*#-code-walkthrough(1.introView)*/ IntroView: /*#-code-walkthrough(1.introView)*/ View {
-    /*#-code-walkthrough(1.body)*/
+struct IntroView: View {
     var body: some View {
-        //#-learning-task(placeOneViewInsideAnother)
-        //#-learning-task(addImageInHStack)
-        //#-learning-task(modifierWalkthrough)
-        /*#-code-walkthrough(2.bothModifiers)*/
-        //#-learning-task(addFriend)
-        //#-learning-task(modifier)
-        /*#-code-walkthrough(2.bothModifiers)*/
-        //#-learning-task(changeText)
-        /*#-code-walkthrough(1.text)*/
         HStack {
             Image("FriendAndGem")
                 .resizable()
@@ -23,13 +12,9 @@ struct/*#-code-walkthrough(1.introView)*/ IntroView: /*#-code-walkthrough(1.intr
         
         Text("Hello, world!")
         Text("This is a string!")
-                /*#-code-walkthrough(1.text)*/
-        //#-learning-task(textElement)
     }
-    /*#-code-walkthrough(1.body)*/
 }
 
-/*#-code-walkthrough(1.preview)*/
 struct IntroView_Previews: PreviewProvider {
     static var previews: some View {
         VStack {
@@ -37,6 +22,3 @@ struct IntroView_Previews: PreviewProvider {
         }
     }
 }
-/*#-code-walkthrough(1.preview)*/
-/*#-code-walkthrough(1.first)*/
-
