b2adc227c1accf76b051e9fa42496431ea442374
Jimmy-Sim <77947780+Jimmy-Sim@users.noreply.github.com>
2023-07-14 09:07:37 +0900

    Create FriendDetailView.swift

---
+++ Get Started with Apps/FriendDetailView.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+struct FriendDetailView: View {
+    var body: some View {
+        VStack {
+        //#-learning-task(composeAView)
+        //#-learning-task(addTextInVStack)
+        //#-learning-task(describeFriend)
+            HStack {
+                Image("Friend")
+                    .resizable()
+                    .scaledToFit()
+                
+                VStack {
+                    Text("Friend")
+                        .font(.largeTitle)
+                    Text("Friend is orange!")
+                        .font(.caption)
+                }
+            }
+        }
+    }
+}
+
+struct FriendDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            FriendDetailView()
+        }
+    }
+}
