4e4ff1c6482cb9a23a29502fe577f4b81152de1f
Jimmy-Sim <77947780+Jimmy-Sim@users.noreply.github.com>
2023-07-14 09:07:22 +0900

    Create ExperimentView.swift

---
+++ Get Started with Apps/ExperimentView.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+
+struct ExperimentView: View {
+    var body: some View {
+        VStack {
+            FriendDetailView()
+            HStack {
+                Image("Blu")
+                    .resizable()
+                    .scaledToFit()
+                
+                VStack {
+                    Text("Blu")
+                        .font(.largeTitle)
+                    Text("Blu is blue!")
+                        .font(.caption)
+                }
+            }
+            
+            HStack {
+                Image("Hopper")
+                    .resizable()
+                    .scaledToFit()
+                
+                VStack {
+                    Text("Hopper")
+                        .font(.largeTitle)
+                    Text("Hopper is green!")
+                        .font(.caption)
+                }
+            }
+            //#-learning-task(createBluView)
+            //#-learning-task(createHopperView)
+        }
+    }
+}
+
+struct ExperimentView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            ExperimentView()
+        }
+    }
+}
