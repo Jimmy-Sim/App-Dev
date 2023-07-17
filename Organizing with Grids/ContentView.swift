99575d389f7c7db43f7c3e3b9f6e3562672fdb46
Jimmy-Sim <77947780+Jimmy-Sim@users.noreply.github.com>
2023-07-17 09:34:03 +0900

    Create ContentView.swift

---
+++ Organizing with Grids/ContentView.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selectedColor = Color.white
+    
+    let columnLayout = Array(repeating:  GridItem(.flexible(minimum: 20, maximum: 100)) , count: 3)
+    let customColumns = [GridItem(.fixed(75)), GridItem(.fixed(100)), GridItem(.flexible(minimum: 125, maximum: 250))]
+    let adaptiveColumns = [GridItem(.adaptive(minimum: 100, maximum: 250))]
+    
+    let allColors: [Color] = [.pink, .red, .orange, .yellow, .green, .mint, .teal, .cyan, .blue, .indigo, .purple, .brown, .gray]
+    
+    var body: some View {
+        ScrollView {
+            Image("cactuswren")
+                .resizable()
+                .scaledToFit()
+                .colorMultiply(selectedColor)
+            LazyVGrid(columns: adaptiveColumns) {
+                ForEach(allColors.indices, id: \.self) { index in
+                    Button {
+                        selectedColor = allColors[index]
+                    } label: {
+                        RoundedRectangle(cornerRadius: 4.0)
+                            .aspectRatio(1.0, contentMode: ContentMode.fit)
+                            .foregroundColor(allColors[index])
+                    }
+                }
+            }
+        }
+        .padding()
+    }
+}
