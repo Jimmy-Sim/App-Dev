acc04d257609f6131de5925807eb20b47bb9bbb6
Jimmy-Sim <77947780+Jimmy-Sim@users.noreply.github.com>
2023-07-26 09:58:07 +0900

    Create Bindings.swift

---
+++ Keep Going with Apps/Bindings.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+struct Bindings: View {
+    @State var isOn = false
+    @State var color = Color.primary
+    
+    var body: some View {
+        VStack {
+            Toggle("Press Me", isOn: $isOn)
+            
+            ColorPicker("Select a Color", selection: $color)
+            
+            Image(systemName: isOn ? "battery.100" : "battery.25")
+                .font(.system(size: 150))
+                .foregroundColor(color)
+            
+            Text("Hello world!")
+                .font(.largeTitle)
+                .foregroundColor(color)
+        }
+        .padding()
+    }
+}
+
+struct Bindings_Previews: PreviewProvider {
+    static var previews: some View {
+        Bindings().assess()
+    }
+}
