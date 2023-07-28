f48c1f98aa92ff7cca2f08e115b859910e0667ba
Jimmy-Sim <77947780+Jimmy-Sim@users.noreply.github.com>
2023-07-28 11:34:00 +0900

    Create NavigationExperiment.swift

---
+++ Keep Going with Apps/NavigationExperiment.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct NavigationExperiment: View {
+    var body: some View {
+        VStack {
+            NavigationStack { 
+                List { 
+                    Text("NavigationStack Content")
+                    NavigationLink("Push Me!") { 
+                        Text("I'm the destination View")
+                            .navigationTitle("Destination")
+                    }
+                    NavigationLink("No, Push Me!") { 
+                        Text("I'm another destination View")
+                            .navigationTitle("Destination")
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct NavigationExperiment_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationExperiment().assess()
+    }
+}
